759c065cafbebbc105b426016adcff0ab673914b
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-06 16:21:35 -0300

    4° commit - tdd model: description consegue ser preenchido? RED

--- spec/models/heroes_spec.rb
+++ spec/models/heroes_spec.rb
@@ -5,10 +5,17 @@ RSpec.describe Hero, type: :model do
   before{@hero = Hero.new}
 
   describe 'Testes de preenchimento do model Hero' do
+
     it 'name consegue ser preenchido?' do
       @hero.name = 'spiderman'
       expect(@hero.name).to eq('spiderman')
     end    
+
+    it 'description consegue ser preenchido?' do
+      @hero.description = ''
+      expect(@hero.description).to eq('inimigo do venon')
+    end
+
   end
 
 end
